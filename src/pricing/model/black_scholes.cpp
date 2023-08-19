8f4921a60cad05a8faa68a41b5490f43ae8083f8
Matteo Durante <waifod@proton.me>
2023-08-19 21:12:04 +0200

    Extend testing via main.cpp and go forth with black_scholes implementation (#9)

--- src/pricing/model/black_scholes.cpp
+++ src/pricing/model/black_scholes.cpp
@@ -12,10 +12,19 @@
 
 BlackScholes::BlackScholes(std::shared_ptr<MarketDataProvider> market_data_provider)
 noexcept
-: Model(market_data_provider) {}
+:
+Model(market_data_provider) {}
 
 double BlackScholes::GetAssetPrice(std::shared_ptr<Asset> asset) const
 noexcept {
   double sigma = market_data_provider_->GetAssetVolatility(asset);
+  double spot_price = market_data_provider_->GetAssetSpotPrice(asset);
+  double interest_rate = market_data_provider_->GetInterestRate();
+  double strike_price = 2.0;  // TODO: Get the strike price
+  int time_to_maturity = 2;  // TODO: Get the time to maturity
+  double d_one = (std::log(spot_price / strike_price) +
+      (interest_rate + (sigma * sigma / 2) * time_to_maturity)) /
+      (sigma * std::sqrt(time_to_maturity));
+  double d_two = d_one - sigma*std::sqrt(time_to_maturity);
   return sigma;
 }
