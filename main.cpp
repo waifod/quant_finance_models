92e9457d9c203c0ff795598f3d34f7e550408316
David Álvarez Rosa <david@alvarezrosa.com>
2023-09-15 16:31:07 +0200

    Link against cURL library for the project
    Added cURL library to the project.
    
    Testing
    -------
    
    Added a usage example in the `main.cpp` file.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <curl/curl.h>
+
 #include <boost/math/distributions/normal.hpp>
 #include <iostream>
 
@@ -12,9 +14,31 @@
 #include "qfm/pricing/model/black_scholes.hpp"
 #include "qfm/pricing/pricing.hpp"
 
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                            void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
 int main() {
   std::cout << "Hello World!" << std::endl;
 
+  std::cout << "Testing cURL library" << std::endl;
+  // Example from https://curl.se/libcurl/c/curl_easy_init.html
+  // modified as in https://gist.github.com/alghanmi/c5d7b761b2c9ab199157
+  CURL *curl = curl_easy_init();
+  if (curl) {
+    CURLcode res;
+    std::string readBuffer;
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    std::cout << readBuffer << std::endl;
+  }
+
+  std::cout << "Testing Boost math library" << std::endl;
   auto gaussian = boost::math::normal_distribution(0, 1);
   std::cout << "Outputting random value: " << cdf(gaussian, 2) << std::endl;
 
